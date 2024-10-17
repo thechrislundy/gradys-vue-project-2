8fd7aa47a6fc03a9e14ffb510f301e00fe11aea2
Chris Lundy <17070542+thechrislundy@users.noreply.github.com>
2024-10-17 16:18:55 +0000

    added bootstrap and other packages from old project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      deep: true,
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [IconsResolver()]
+    }),
+    Icons({
+      scale: 1,
+      defaultStyle: 'vertical-align: -0.125em;', // Style apply to icons
+      defaultClass: 'icon'
+    })
   ],
   resolve: {
     alias: {
@@ -14,3 +30,4 @@ export default defineConfig({
     }
   }
 })
+
