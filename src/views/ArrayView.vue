ea61a69d61d16da28226c5619d4b931b04670f75
Grady Klingler <gradyklingler@gmail.com>
2024-10-23 22:01:45 +0000

    Refactor Array

--- src/views/ArrayView.vue
+++ src/views/ArrayView.vue
@@ -1,43 +1,4 @@
 <template>
-  <div class="my-5 card text-center">
-    <h2 class="card-header">Counter Example</h2>
-    <div class="card-body">
-      <div class="row my-4">
-        <div class="col-sm-6 col-lg-3 my-2">
-          <button @click="add" class="btn btn-primary">Add to count</button>
-        </div>
-        <div class="col-sm-6 col-lg-3 my-2">
-          <button @click="subtract" class="btn btn-primary">
-            Subtract to count
-          </button>
-        </div>
-        <div class="col-sm-6 col-lg-3 my-2">
-          <div class="input-group">
-            <input
-              type="number"
-              class="form-control"
-              v-model="amount"
-              @keyup.enter="addAmount"
-            />
-            <button
-              @click="addAmount"
-              class="btn btn-primary"
-              type="button"
-              id="button-addon2"
-            >
-              Add {{ amount }} to count
-            </button>
-          </div>
-        </div>
-        <div class="col-sm-6 col-lg-3 my-2">
-          <button @click="count = 0" class="btn btn-secondary">
-            Reset count
-          </button>
-        </div>
-      </div>
-    </div>
-    <h1 class="card-footer text-body-secondary m-0">Count: {{ count }}</h1>
-  </div>
   <div class="col-sm-6 col-lg-3 my-2">
     <div class="input-group">
       <input
@@ -100,8 +61,6 @@ import draggable from "vuedraggable";
 export default {
   data() {
     return {
-      count: 0,
-      amount: 0,
       list: [],
       item: "",
       sortMode: null,
@@ -112,15 +71,6 @@ export default {
   },
 
   methods: {
-    add() {
-      this.count++;
-    },
-    subtract() {
-      this.count--;
-    },
-    addAmount() {
-      this.count = this.count + this.amount;
-    },
     addToList() {
       this.list.push({
         string: this.item,
