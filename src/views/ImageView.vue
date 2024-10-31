d9440ebed2f24fae6aa187079f040aa7e8be9175
Grady Klingler <gradyklingler@gmail.com>
2024-10-31 22:08:24 +0000

    Added images slides

--- src/views/ImageView.vue
+++ src/views/ImageView.vue
@@ -1,12 +1,24 @@
-<template>{{ images }}</template>
+<template>
+  <VueTinySlider :options="options">
+    <div>Slide #1</div>
+    <div>Slide #2</div>
+    <div>Slide #3</div> </VueTinySlider
+  >{{ images }}
+</template>
 <script>
+import VueTinySlider from "@mentorkadriu/vue-tiny-slider";
+import "tiny-slider/src/tiny-slider.scss";
 export default {
   data() {
     return {
       images: [],
+      options: {
+        items: 1,
+        speed: 400,
+      },
     };
   },
-
+  components: { VueTinySlider },
   methods: {
     fetchImages() {
       fetch("https://jsonplaceholder.typicode.com/photos")
